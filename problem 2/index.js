8bc34c0964abd12063c0350ec8c3730c98b03726
HRISHIKESH BHAGAT <hrishibhagat08@gmail.com>
2022-08-17 18:22:12 +0530

    express heroku api

--- problem 2/index.js
+++ problem 2/index.js
@@ -1,12 +1,12 @@
 const data = require("./student/getDetails.json");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 1010
-app.get("/",(req,res)=>{
-    res.send("api is running..");
-})
+const port = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("api is running..");
+});
 
 app.post("/student/getDetails", function (req, res) {
-    res.send(data);
+  res.send(data);
 });
-app.listen(port, console.log(`server runnig..... `));
+app.listen(port, console.log(`server runnig on port ${port} `));
