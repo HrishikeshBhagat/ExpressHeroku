4641fa76e69b3668975ec357bacfa33557fd6a64
HRISHIKESH BHAGAT <hrishibhagat08@gmail.com>
2022-08-17 23:08:52 +0530

    Express Heroku

--- problem 1/index.js
+++ problem 1/index.js
@@ -1,6 +1,9 @@
 const data = require("./student/getDetails.json");
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  res.send("api is running so far");
+});
 
 const PORT = 8000;
 app.post("/student/getDetails", function (req, res) {
--- problem 2/index.js
+++ problem 2/index.js
@@ -1,12 +1,12 @@
 const data = require("./student/getDetails.json");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 app.get("/", (req, res) => {
-  res.send("api is running..");
+  res.send("api is running...");
 });
 
-app.post("/student/getDetails", function (req, res) {
+app.get("/student/getDetails", function (req, res) {
   res.send(data);
 });
 app.listen(port, console.log(`server runnig on port ${port} `));
